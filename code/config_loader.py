f7522bb5a166f2b54f76d11690d7ad5daf68a5a1
Abhishek Patel <155148518+pabhishek101@users.noreply.github.com>
2024-09-04 13:09:29 +0530

    Update config_loader.py

--- code/config_loader.py
+++ code/config_loader.py
@@ -1,6 +1,6 @@
 import yaml
 
-def load_config(config_file="code\config.yaml"):
+def load_config(config_file="config.yaml"):
     with open(config_file, "r") as file:
         config = yaml.safe_load(file)
     return config
